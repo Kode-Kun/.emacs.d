5cd64bba8d5c8e51f073fccbeb2c64e7b560d82e
Lui Sant'Ana Cardoso <lusac2@ulaval.ca>
2025-11-19 15:20:25 -0500

    added evil-mode specific keybinds to kode-mode

--- kode-mode.el
+++ kode-mode.el
@@ -22,13 +22,28 @@
   (kode-mode -1))
 (add-hook 'minibuffer-setup-hook 'turn-off-kode-mode)
 
+;; here's where my custom keybindings, applied to all buffers, reside.
 (define-key kode-mode-map (kbd "C-c C-c") 'compile)
 (define-key kode-mode-map (kbd "C-c C-f") 'comment-line)
 (define-key kode-mode-map (kbd "C-c C-d") 'uncomment-region)
 (define-key kode-mode-map (kbd "C-c C-s") 'comment-indent)
 (define-key kode-mode-map (kbd "C-x C-h") 'previous-buffer)
 (define-key kode-mode-map (kbd "C-x C-l") 'next-buffer)
-(define-key kode-mode-map (kbd "M-j") 'move-text-down)
-(define-key kode-mode-map (kbd "M-k") 'move-text-up)
+(define-key kode-mode-map (kbd "C-x C-s") 'scratch-buffer)
+
+;; unbinding certain annoying keybindings (I use C-x C-c way too often for
+;; it to kill emacs if I'm in the wrong evil stage...
+(define-key global-map (kbd "C-x C-c") 'nil)
+
+;; keybindings applied only in normal mode (evil stage)
+(evil-define-key 'normal kode-mode-map (kbd "M-j")     'move-text-down)
+(evil-define-key 'normal kode-mode-map (kbd "M-k")     'move-text-up)
+
+;; keybindings applied only in insert mode
+(evil-define-key 'insert kode-mode-map (kbd "C-x C-c") 'evil-normal-state)
+(evil-define-key 'insert kode-mode-map (kbd "M-h") 'evil-backward-char)
+(evil-define-key 'insert kode-mode-map (kbd "M-j") 'evil-next-line)
+(evil-define-key 'insert kode-mode-map (kbd "M-k") 'evil-previous-line)
+(evil-define-key 'insert kode-mode-map (kbd "M-l") 'evil-forward-char)
 
 (provide 'kode-mode)
